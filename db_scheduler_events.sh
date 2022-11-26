894fe416762e82178e523263ddb79badad1cd506
TamerB <tamer.bhgt@gmail.com>
2022-11-26 04:09:14 +0200

    Add cron jobs

---
+++ db_scheduler_events.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+mysql --user=$MYSQL_USER --password=$MYSQL_PASS --database=$MYSQL_DEV_DB --host=$MYSQL_DEV_HOST --port=$MYSQL_DEV_PORT --execute="
+CREATE EVENT update_applications_messages_number
+ON SCHEDULE EVERY 59 MINUTE
+STARTS (SELECT NOW() LIMIT 1)
+DO
+UPDATE applications SET chats_number = (SELECT COUNT(*) FROM chats WHERE chats.token = applications.token LIMIT 1);"
+
+mysql --user=$MYSQL_USER --password=$MYSQL_PASS --database=$MYSQL_DEV_DB --host=$MYSQL_DEV_HOST --port=$MYSQL_DEV_PORT --execute="
+CREATE EVENT update_chats_messages_number
+ON SCHEDULE EVERY 59 MINUTE
+STARTS (SELECT NOW() LIMIT 1)
+DO
+UPDATE chats SET messages_number = (SELECT COUNT(*) FROM messages WHERE messages.token = chats.token && messages.chat_number = chats.number LIMIT 1);"
