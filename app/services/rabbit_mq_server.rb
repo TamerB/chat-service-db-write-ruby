3099975eed14b2a30a7e1169bd77b9640315a85e
TamerB <tamer.bhgt@gmail.com>
2022-12-08 23:16:51 +0200

    Add race conditions testing & refactor handler

--- app/services/rabbit_mq_server.rb
+++ app/services/rabbit_mq_server.rb
@@ -1,119 +1,177 @@
-class RabbitMqServer < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: -> {return {data: 'not found', status: 404}}
-    def initialize
-        @connection = Bunny.new(host: ENV.fetch('MQ_HOST'), automatically_recover: false)
-        @connection.start
-        @channel = @connection.create_channel
-    end
-    
-    def start(queue_name)
-        @channel = $channel
-        @connection = $connection
-        @queue = channel.queue(queue_name)
-        @exchange = channel.default_exchange
-        subscribe_to_queue
-    end
-    
-    def stop
-        channel.close
-        connection.close
-    end
-    
-    def loop_forever
-        # This loop only exists to keep the main thread
-        # alive. Many real world apps won't need this.
-        loop { sleep 5 }
-    end
-    
-    private
-    
-    attr_reader :channel, :exchange, :queue, :connection
-    
-    def subscribe_to_queue
-        queue.subscribe do |_delivery_info, properties, payload|
-            result = forward_action(JSON.parse(payload))
-            exchange.publish(
-            result.to_json,
-            routing_key: properties.reply_to,
-            correlation_id: properties.correlation_id
-            )
-        end
-    end
+require 'time'
 
-    def forward_action(value)
-        begin
-            case value['action']
-            when 'application.create'
-                logger.info "Application create started: #{value['params']}"
-                application = Application.new(name: value['params']['name'])
-                if application.save
-                    logger.info "Application show completed: #{value['params']}"
-                    return {data: application, status: 201}
-                else
-                    logger.warn "Application show cancelled (name is required): #{value['params']}"
-                    return {data: 'name is required', status: 400}
-                end
-            when 'application.update'
-                logger.info "Application update started: #{value['params']}"
-                application = Application.find(value['params']['token'])
-                if application.update(value['params']['application'])
-                    logger.info "Application update completed: #{value['params']}"
-                    return {data: application, status: 200}
-                else
-                    logger.warn "Application update cancelled (unprocessed entity): #{value['params']}"
-                    return {data: 'unprocessed entity', status: 422}
-                end
-            when 'chat.create'
-                logger.info "Chat create started: #{value['params']}"
-                application = Application.where(token: value['params']).first
-                if application.nil?
-                    logger.warn "Chat create cancelled (application not found): #{value['params']}"
-                    return {data: 'application not found', status: 404}
-                end
-                chat = Chat.new(token: application.token)
-                if chat.save
-                    logger.info "Chat create completed: #{value['params']}"
-                    return {data: chat, status: 201}
-                else
-                    logger.warn "Chat create cancelled (token is required): #{value['params']}"
-                    return {data: 'token is required', status: 400}
-                end
-            when 'message.create'
-                logger.info "Message create started: #{value['params']}"
-                chat = Chat.where(token: value['params']['application_token'], number: value['params']['chat_number']).first
-                if chat.nil?
-                    logger.warn "Message create cancelled (chat not found): #{value['params']}"
-                    return {data: "chat not found", status: 404}
-                end
-                message = Message.new(token: chat.token, chat_number: chat.number, body: value['params']['body'])
-                if message.save
-                    logger.info "Message create completed: #{value['params']}"
-                    return {data: message, status: 201}
-                else
-                    logger.warn "Message create cancelled (required data missing): #{value['params']}"
-                    return {data: 'one or more required field is missing', status: 400}
-                end
-            when 'message.update'
-                logger.info "Message update started: #{value['params']}"
-                message = Message.where(token: value['params']['application_token'], chat_number: value['params']['chat_number'], number: value['params']['number']).first
-                if message.nil?
-                    logger.warn "Message update cancelled (message not found): #{value['params']}"
-                    return {data: "message not found", status: 404}
-                end
-                if message.update(value['params']['message'])
-                    logger.info "Message update completed: #{value['params']}"
-                    return {data: message, status: 200}
-                else
-                    logger.warn "Message update cancelled (unprocessed entity): #{value['params']}"
-                    return {data: 'unprocessed entity', status: 422}
-                end
-            else
-                logger.error "Bad request: Unrecognized operation"
-                return {data: 'unrecognized operation', status: 400}
-            end
-        rescue Exception => e
-            logger.error e.message
-            return {data: 'something went wrong', status: 500}
-        end
+class RabbitMqServer < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: -> {return {data: 'not found', status: 404}}
+  def initialize
+    @connection = Bunny.new(host: ENV.fetch('MQ_HOST'), automatically_recover: false)
+    @connection.start
+    @channel = @connection.create_channel
+  end
+  
+  def start(queue_name)
+    @channel = $channel
+    @connection = $connection
+    @queue = channel.queue(queue_name)
+    @exchange = channel.default_exchange
+    subscribe_to_queue
+  end
+  
+  def stop
+    channel.close
+    connection.close
+  end
+  
+  def loop_forever
+    # This loop only exists to keep the main thread
+    # alive. Many real world apps won't need this.
+    loop { sleep 5 }
+  end
+
+  def forward_action(value)
+    case value['action']
+    when 'application.create'
+      return create_application(value['params'])
+    when 'application.update'
+      return update_application(value['params'])
+    when 'chat.create'
+      return create_chat(value['params'])
+    when 'message.create'
+      return create_message(value['params'])
+    when 'message.update'
+      return update_message(value['params'])
+    else
+      logger.error "Bad request (#{value['action']}): Unrecognized operation"
+      return {data: 'Something went wrong. Please try again later', status: 500}
     end
+  end
+  
+  private
+  
+  attr_reader :channel, :exchange, :queue, :connection
+  
+  def subscribe_to_queue
+    queue.subscribe do |_delivery_info, properties, payload|
+      result = forward_action(JSON.parse(payload))
+      exchange.publish(
+        result.to_json,
+        routing_key: properties.reply_to,
+        correlation_id: properties.correlation_id
+      )
+    end
+  end
+
+  def create_application(value)
+    begin
+      logger.info "Application create started: #{value}"
+      application = Application.new(name: value['name'])
+      if application.save
+        logger.info "Application show completed: #{value}"
+        return {data: application, status: 201}
+      else
+        logger.warn "Application show cancelled (name is required): #{value}"
+        return {data: 'unprocessed entity', status: 400}
+      end
+    rescue Exception => e
+      return catch_error('application create', e.message)
+    end
+  end
+
+  def update_application(value)
+    begin
+      logger.info "Application update started: #{value}"
+      application = Application.find(value['token'])
+      if application.update(value['application'])
+        logger.info "Application update completed: #{value}"
+        return {data: application, status: 200}
+      else
+        logger.warn "Application update cancelled (unprocessed entity): #{value}"
+        return {data: 'unprocessed entity', status: 422}
+      end
+    rescue Exception => e
+      return catch_error('application update', e.message)
+    end
+  end
+
+  def create_chat(value)
+    begin
+      logger.info "Chat create started: #{value}"
+      application = Application.where(token: value).first
+      if application.nil?
+        logger.warn "Chat create cancelled (application not found): #{value}"
+        return {data: 'application not found', status: 404}
+      end
+      
+      chat = Chat.new(token: application.token)
+      before = Time.now()
+      loop do
+        begin
+          if chat.save
+            logger.info "Chat create completed: #{value}"
+            return {data: chat, status: 201}
+          end
+        rescue Exception => e
+          if Time.now() - before > 1000.0
+            logger.warn "Chat create cancelled #{e.message}: #{value}"
+            return {data: 'unprocessed entity', status: 400}
+          end
+        end
+      end
+    rescue Exception => e
+      return catch_error('Chat create', e.message)
+    end
+  end
+
+  def create_message(value)
+    begin
+      logger.info "Message create started: #{value}"
+      chat = Chat.where(token: value['application_token'], number: value['chat_number']).first
+      if chat.nil?
+        logger.warn "Message create cancelled (chat not found): #{value}"
+        return {data: "chat not found", status: 404}
+      end
+      message = Message.new(token: chat.token, chat_number: chat.number, body: value['body'])
+      
+      before = Time.now()
+      loop do
+        begin
+          if message.save
+            logger.info "Message create completed: #{value}"
+            return {data: message, status: 201}
+          end
+        rescue Exception => e
+          if Time.now() - before > 1000.0
+            logger.warn "Message create cancelled (#{e.message}): #{value}"
+            return {data: 'Something went wrong. Please try again later', status: 500}
+          end
+        end
+      end
+    rescue Exception => e
+      return catch_error('Message create', e.message)
+    end
+  end
+
+  def update_message(value)
+    begin
+      logger.info "Message update started: #{value}"
+      message = Message.where(token: value['application_token'], chat_number: value['chat_number'], number: value['number']).first
+      if message.nil?
+        logger.warn "Message update cancelled (message not found): #{value}"
+        return {data: "message not found", status: 404}
+      end
+      if message.update(value['message'])
+        logger.info "Message update completed: #{value}"
+        return {data: message, status: 200}
+      else
+        logger.warn "Message update cancelled (unprocessed entity): #{value}"
+        return {data: 'unprocessed entity', status: 422}
+      end
+    rescue Exception => e
+      return catch_error('Message update', e.message)
+    end
+  end
+
+  def catch_error(prefix, err)
+    logger.error "#{prefix} cancelled: #{err}"
+    return {data: 'something went wrong. Please try again later', status: 500}
+  end
 end
---
+++ test/services/rabbit_mq_server_test.rb
@@ -0,0 +1,79 @@
+require "test_helper"
+
+class RabbitMqServerTest < ActiveSupport::TestCase
+  test 'shoud create 5 different chats' do
+    begin
+      server = RabbitMqServer.new
+      concurrency_level = 5
+      chats = []
+      threads = concurrency_level.times.map do |i|
+        Thread.new do
+          chat = server.forward_action(JSON.parse({ action: 'chat.create', params: 'abcdef' }.to_json))
+          chats << chat[:data]
+        end
+      end
+      threads.each(&:join)
+      assert chats.count == 5, "Should return 5 chats, returned #{chats.count} chats"
+      assert chats[0].number != chats[1].number, "chat 0 number should not equal chat 1 number: #{chats[0].number}"
+      assert chats[0].number != chats[2].number, "chat 0 number should not equal chat 2 number: #{chats[0].number}"
+      assert chats[0].number != chats[3].number, "chat 0 number should not equal chat 3 number: #{chats[0].number}"
+      assert chats[0].number != chats[4].number, "chat 0 number should not equal chat 4 number: #{chats[0].number}"
+      assert chats[1].number != chats[0].number, "chat 1 number should not equal chat 0 number: #{chats[1].number}"
+      assert chats[1].number != chats[2].number, "chat 1 number should not equal chat 2 number: #{chats[1].number}"
+      assert chats[1].number != chats[3].number, "chat 1 number should not equal chat 3 number: #{chats[1].number}"
+      assert chats[1].number != chats[4].number, "chat 1 number should not equal chat 4 number: #{chats[1].number}"
+      assert chats[2].number != chats[1].number, "chat 2 number should not equal chat 1 number: #{chats[2].number}"
+      assert chats[2].number != chats[0].number, "chat 2 number should not equal chat 0 number: #{chats[2].number}"
+      assert chats[2].number != chats[3].number, "chat 2 number should not equal chat 3 number: #{chats[2].number}"
+      assert chats[2].number != chats[4].number, "chat 2 number should not equal chat 4 number: #{chats[2].number}"
+      assert chats[3].number != chats[1].number, "chat 3 number should not equal chat 1 number: #{chats[3].number}"
+      assert chats[3].number != chats[2].number, "chat 3 number should not equal chat 2 number: #{chats[3].number}"
+      assert chats[3].number != chats[0].number, "chat 3 number should not equal chat 0 number: #{chats[3].number}"
+      assert chats[3].number != chats[4].number, "chat 3 number should not equal chat 4 number: #{chats[3].number}"
+      assert chats[4].number != chats[1].number, "chat 4 number should not equal chat 1 number: #{chats[4].number}"
+      assert chats[4].number != chats[2].number, "chat 4 number should not equal chat 2 number: #{chats[4].number}"
+      assert chats[4].number != chats[3].number, "chat 4 number should not equal chat 3 number: #{chats[4].number}"
+      assert chats[4].number != chats[0].number, "chat 4 number should not equal chat 0 number: #{chats[4].number}"
+    ensure
+      ActiveRecord::Base.connection_pool.disconnect!
+    end
+  end
+
+  test 'shoud messages 5 different chats' do
+    begin
+      server = RabbitMqServer.new
+      concurrency_level = 5
+      messages = []
+      threads = concurrency_level.times.map do |i|
+        Thread.new do
+          message = server.forward_action(JSON.parse({ action: 'message.create', params: { application_token: 'abcdef', chat_number: 1, body: 'hi' } }.to_json))
+          messages << message[:data]
+        end
+      end
+      threads.each(&:join)
+      assert messages.count == 5, "Should return 5 messages, returned #{messages.count} messages"
+      assert messages[0].number != messages[1].number, "message 0 number should not equal message 1 number: #{messages[0].number}"
+      assert messages[0].number != messages[2].number, "message 0 number should not equal message 2 number: #{messages[0].number}"
+      assert messages[0].number != messages[3].number, "message 0 number should not equal message 3 number: #{messages[0].number}"
+      assert messages[0].number != messages[4].number, "message 0 number should not equal message 4 number: #{messages[0].number}"
+      assert messages[1].number != messages[0].number, "message 1 number should not equal message 0 number: #{messages[0].number}"
+      assert messages[1].number != messages[2].number, "message 1 number should not equal message 2 number: #{messages[0].number}"
+      assert messages[1].number != messages[3].number, "message 1 number should not equal message 3 number: #{messages[0].number}"
+      assert messages[1].number != messages[4].number, "message 1 number should not equal message 4 number: #{messages[0].number}"
+      assert messages[2].number != messages[1].number, "message 2 number should not equal message 1 number: #{messages[0].number}"
+      assert messages[2].number != messages[0].number, "message 2 number should not equal message 0 number: #{messages[0].number}"
+      assert messages[2].number != messages[3].number, "message 2 number should not equal message 3 number: #{messages[0].number}"
+      assert messages[2].number != messages[4].number, "message 2 number should not equal message 4 number: #{messages[0].number}"
+      assert messages[3].number != messages[1].number, "message 3 number should not equal message 1 number: #{messages[0].number}"
+      assert messages[3].number != messages[2].number, "message 3 number should not equal message 2 number: #{messages[0].number}"
+      assert messages[3].number != messages[0].number, "message 3 number should not equal message 0 number: #{messages[0].number}"
+      assert messages[3].number != messages[4].number, "message 3 number should not equal message 4 number: #{messages[0].number}"
+      assert messages[4].number != messages[1].number, "message 4 number should not equal message 1 number: #{messages[0].number}"
+      assert messages[4].number != messages[2].number, "message 4 number should not equal message 2 number: #{messages[0].number}"
+      assert messages[4].number != messages[3].number, "message 4 number should not equal message 3 number: #{messages[0].number}"
+      assert messages[4].number != messages[0].number, "message 4 number should not equal message 0 number: #{messages[0].number}"
+    ensure
+      ActiveRecord::Base.connection_pool.disconnect!
+    end
+  end
+end
